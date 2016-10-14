490e987b3ca9cdd4e0f8145c8318c9058844aaac
Petros Kataras <petroskataras@gmail.com>
2016-10-14 16:11:45 +0200

    Small receiver refactor to allow runtime autoconnection with a newly availble source.

--- src/CinderNDIReceiver.cpp
+++ src/CinderNDIReceiver.cpp
@@ -20,9 +20,13 @@ CinderNDIReceiver::CinderNDIReceiver()
 
 CinderNDIReceiver::~CinderNDIReceiver()
 {
+	if( mNdiFinder ) {
+		NDIlib_find_destroy( mNdiFinder );	
+	}
 	if( mNdiReceiver ) {
 		NDIlib_recv_destroy( mNdiReceiver );
 	}
+
 	NDIlib_destroy();
 	mNdiInitialized = false;
 }
@@ -32,23 +36,30 @@ void CinderNDIReceiver::setup()
 	if( mNdiInitialized ) {
 		const NDIlib_find_create_t NDI_find_create_desc = { true, nullptr };
 
-		auto ndiFinder = NDIlib_find_create( &NDI_find_create_desc );
-		if( ! ndiFinder ) {
+		mNdiFinder = NDIlib_find_create( &NDI_find_create_desc );
+		if( ! mNdiFinder ) {
 			CI_LOG_E( "Failed to create NDI finder!" );
 		}
 		
 		unsigned int no_sources = 0;
 		const NDIlib_source_t* p_sources = nullptr;
 		while( ! no_sources ) {
-			p_sources = NDIlib_find_get_sources( ndiFinder, &no_sources, 10000 );
+			mNdiSources = NDIlib_find_get_sources( mNdiFinder, &no_sources, 10000 );
 		}
 
+		initConnection();
+	}
+}
+
+void CinderNDIReceiver::initConnection()
+{
+	if( mNdiSources ) {
 		NDIlib_recv_create_t NDI_recv_create_desc = 
 		{
-		   	p_sources[0],
-		 	false,
+			mNdiSources[0],
+			false,
 			NDIlib_recv_bandwidth_highest,
-		   	true 
+			true 
 		};
 
 		mNdiReceiver = NDIlib_recv_create2( &NDI_recv_create_desc );
@@ -56,7 +67,6 @@ void CinderNDIReceiver::setup()
 			CI_LOG_E( "Failed to create NDI receiver!" );
 		}
 
-		NDIlib_find_destroy( ndiFinder );	
 		const NDIlib_tally_t tally_state = { true, false };
 		NDIlib_recv_set_tally( mNdiReceiver, &tally_state);
 
@@ -66,12 +76,35 @@ void CinderNDIReceiver::setup()
 
 void CinderNDIReceiver::update()
 {
+	// Check if we have at least one source
+	unsigned int no_sources = 0;
+	const NDIlib_source_t* p_sources = nullptr;
+	mNdiSources = NDIlib_find_get_sources( mNdiFinder, &no_sources, 0 );
+
+	if( ! no_sources ) {
+		mReadToReceive = false;
+		// Connections might take a while.. Wait for 10secs..
+		while( ! no_sources ) {
+			mNdiSources = NDIlib_find_get_sources( mNdiFinder, &no_sources, 10000 );
+		}
+	}
+	else {
+		// If we are here it means that the source has changed
+		// so we need to rebuild our receiver.
+		if( ! mReadToReceive ) {
+			if( mNdiReceiver ) NDIlib_recv_destroy( mNdiReceiver );
+			initConnection();
+		}
+
+	}
+
 	if( mReadToReceive ) {
+
 		NDIlib_video_frame_t video_frame;
 		NDIlib_audio_frame_t audio_frame;
 		NDIlib_metadata_frame_t metadata_frame;
 
-		switch (NDIlib_recv_capture( mNdiReceiver, &video_frame, &audio_frame, &metadata_frame, 1000 ) )
+		switch( NDIlib_recv_capture( mNdiReceiver, &video_frame, &audio_frame, &metadata_frame, 1000 ) )
 		{	
 			// No data
 			case NDIlib_frame_type_none:
